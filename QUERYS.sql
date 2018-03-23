f87df56302f6f8612d8aa95420644de7392d05d3
yohanacano <36299761+yohanacano@users.noreply.github.com>
2018-03-23 08:01:27 -0500

    Update QUERYS.sql

--- QUERYS.sql
+++ QUERYS.sql
@@ -21,11 +21,24 @@ EMPLOYEE_ID NUMBER(*,0) NOT NULL CONSTRAINT FK_SALARY_EMPLOYEE REFERENCES EMPLOY
 PROJECT_ID NUMBER(*,0) CONSTRAINT FK_SALARY_PROJECT REFERENCES PROJECTS(ID) 
 );
 
-SELECT COUNT(*) FROM SALARIES SA
+--Write a SQL query to fetch project code and count of employees (code, employees_count) sorted by employees_count in descending order. example 48205501099, 4
+SELECT PR.CODE AS código, EM.FULL_NAME AS employees_count
+FROM SALARIES SA
 JOIN EMPLOYEES EM ON EM.ID=SA.EMPLOYEE_ID
 JOIN PROJECTS PR ON PR.ID=SA.PROJECT_ID
 WHERE PR.CODE='48205501099';
 
+--Write a query to fetch employee names and their salaries records order by salary asc. Return employee details even if he/she is not assigned to any project
 SELECT EM.FULL_NAME FROM SALARIES SA
 JOIN EMPLOYEES EM ON EM.ID=SA.EMPLOYEE_ID
-WHERE SA.SALARY>=2500 AND SA.SALARY<=3000;
+ORDER BY SA.SALARY ASC;
+
+--Write a SQL query to fetch all the Employees who are also managers from employees table
+
+--Write a SQL query to fetch each employee with the name of his/her boss in one column named "employee_boss" separated by ' - ' (don't forget the spaces). Example: "Drew Rosario - Holmes Manning", "Emery Kelley - Holmes Manning" (employee name - manager name)
+
+--Write a SQL query to fetch all the employees which first name starts with O (uppercase) (Orlando, Ovidio)
+SELECT * FROM EMPLOYEES
+WHERE FULL_NAME LIKE 'O%';
+
+--
